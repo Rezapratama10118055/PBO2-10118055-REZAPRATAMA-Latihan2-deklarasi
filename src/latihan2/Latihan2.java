f6c2d411fb9ec620feb87982dd4423a2311e00f7
User <User@DESKTOP-2RC2UP5>
2019-09-25 07:32:58 +0700

    first commit

---
+++ src/latihan2/Latihan2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan2;
+
+/**
+ *
+ * @author User
+ * NAMA:REZA PRATAMA
+ * KELAS:PBO2
+ * NIM:10118055
+ * Deskripsi:program ini berisi program untuk menampilkan
+ * jenis jenis data bilangan bulat
+ */
+public class Latihan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int nilaiInt;
+    final double PHI=3.14;
+    boolean nilaiLogika;
+    char nilaiKarakter;
+           //memberikan nilai ke variabel
+            nilaiInt = 78;
+            nilaiLogika=false;
+            nilaiKarakter='D';
+            //MENAMPILKAN HASIL
+             System.out.println("isi variabel nilai ="+nilaiInt);
+             System.out.println("isi variabel phi ="+PHI);
+             System.out.println("isi variabel logika ="+nilaiLogika);
+             System.out.println("isi variabel karakter ="+nilaiKarakter);
+    }
+    
+}
